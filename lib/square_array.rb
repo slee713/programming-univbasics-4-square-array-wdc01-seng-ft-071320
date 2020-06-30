352ec8d5b5fe9111623536b3c21edfd20bfadc06
Steven Lee <steven.lee1393@gmail.com>
2020-06-30 17:30:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   # your code here
-  array.length.times do |index|
-    array[index] = array[index]** 2
+  i=0
+  while i<array.length do
+    array[i] = array[i]**2
   end
+  return array
 end
